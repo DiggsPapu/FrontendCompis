6b67bf9116877311316ad0e25d2caee2dcff063f
DiggsPapu <diggs.papu@gmail.com>
2024-05-24 07:38:02 +0000

    delete:Assets

--- src/components/ResponsiveAppBar.jsx
+++ src/components/ResponsiveAppBar.jsx
@@ -5,9 +5,6 @@ import {
 import AdbIcon from '@mui/icons-material/Adb'
 import { Link } from 'react-router-dom'
 import DropdownButton from './dropdownButton'
-import Linkedin from '../assets/linkedin.avif'
-import mail from '../assets/mail.png'
-import github from '../assets/github.svg'
 
 function ResponsiveAppBar() {
   const subject = 'Hi Diego'
@@ -37,9 +34,9 @@ function ResponsiveAppBar() {
             <DropdownButton
               title="CONTACT ME"
               options={
-            [['https://www.linkedin.com/in/diegoalonzomedinilladiggspapu', Linkedin],
-              ['https://github.com/DiggsPapu', github],
-              [`mailto:diego.alonzom@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`, mail],
+            [['https://www.linkedin.com/in/diegoalonzomedinilladiggspapu', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ficon-icons.com%2Ficon%2Flinkedin-black-logo%2F147114&psig=AOvVaw1XNYfWdIib3_5edIv-6AqN&ust=1716622575072000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJim-sjjpYYDFQAAAAAdAAAAABAI'],
+              ['https://github.com/DiggsPapu', 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dgithub&psig=AOvVaw2-jMdrenUNBziwGuFicX5r&ust=1716622604616000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPihwdjjpYYDFQAAAAAdAAAAABAN'],
+              [`mailto:diego.alonzom@gmail.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`, 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.micoope.com.gt%2Fproducto-financiero%2Fseguros%2F%3Fo%3Dmail-icon-in-black-circle-envelope-symbol-vector-image-nn-MY73a0hW&psig=AOvVaw1DmYrXTMM7GEAZSYyqi59Z&ust=1716622633242000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCICWq-XjpYYDFQAAAAAdAAAAABAE'],
             ]
 }
               color="white"
